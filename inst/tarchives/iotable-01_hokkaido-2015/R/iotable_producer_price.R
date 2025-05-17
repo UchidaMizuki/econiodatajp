c6eb03884901f993bbff13affc074d75f10f7f3a
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-05-17 14:46:12 +0900

    Update iotable-01_hokkaido-2015

--- inst/tarchives/iotable-01_hokkaido-2015/R/iotable_producer_price.R
+++ inst/tarchives/iotable-01_hokkaido-2015/R/iotable_producer_price.R
@@ -52,8 +52,8 @@ read_file_iotable_producer_price_medium <- function(file) {
       industry_pattern = "^\\d+_",
       value_added_pattern = "家計外消費支出|雇用者所得|営業余剰|資本減耗引当|間接税|経常補助金",
       final_demand_pattern = "家計外消費支出|民間消費支出|一般政府消費支出|道内総固定資本形成|在庫純増",
-      export_pattern = "^_(輸出|移出)$",
-      import_pattern = "^_（控除）(輸入|移入|関税・輸入品商品税)",
+      export_pattern = "^_[輸移]出$",
+      import_pattern = "^_（控除）([輸移]入|関税・輸入品商品税)",
       total_pattern = "道内生産額"
     ) |>
     io_table_read_data(scale = 1e6) |>
