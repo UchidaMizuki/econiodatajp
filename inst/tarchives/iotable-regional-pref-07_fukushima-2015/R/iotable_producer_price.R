dc13c7f2a25dfc5b387822bcf9e36947f13786d9
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-05-19 23:52:21 +0900

    Update iotable-regional-pref-07_fukushima-2015

--- inst/tarchives/iotable-regional-pref-07_fukushima-2015/R/iotable_producer_price.R
+++ inst/tarchives/iotable-regional-pref-07_fukushima-2015/R/iotable_producer_price.R
@@ -41,7 +41,7 @@ read_file_iotable_producer_price_medium <- function(file) {
       value_added_pattern = "家計外消費支出|賃金・俸給|社会保険料|その他の給与及び手当|営業余剰|資本減耗引当|間接税|経常補助金",
       final_demand_pattern = "家計外消費支出|民間消費支出|一般政府消費支出|県内総固定資本形成|在庫純増",
       export_pattern = "_[移輸]出$",
-      import_pattern = "_（控除）(輸入|関税|輸入品商品税|移入)$",
+      import_pattern = "_（控除）([輸移]入|関税|輸入品商品税)$",
       total_pattern = "県内生産額"
     ) |>
     io_table_read_data(
