836093a26c28f2d4bac76c98fd6647742b73d41c
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-05-25 12:45:20 +0900

    tarchives::use_tarchives() and add dependencies

--- R/econiodatajp-package.R
+++ R/econiodatajp-package.R
@@ -2,5 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dibble dibble
+#' @importFrom econio io_table_regional
+#' @importFrom rlang .data
+#' @importFrom rlang .env
 ## usethis namespace: end
 NULL
