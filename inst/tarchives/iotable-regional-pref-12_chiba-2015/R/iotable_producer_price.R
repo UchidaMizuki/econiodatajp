6648abe88ffd49e2b7b6868da991cf1dc3c6ad91
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-06-01 16:18:29 +0900

    Add iotable-regional-pref-12_chiba and fix saitama data

--- inst/tarchives/iotable-regional-pref-12_chiba-2015/R/iotable_producer_price.R
+++ inst/tarchives/iotable-regional-pref-12_chiba-2015/R/iotable_producer_price.R
@@ -1,11 +1,11 @@
 # iotable_producer_price --------------------------------------------------
 
-# https://www.pref.saitama.lg.jp/a0206/a152/2015io-main.html
+# https://www.pref.chiba.lg.jp/toukei/toukeidata/sangyou/h27/27data.html
 target_iotable_producer_price <- tar_plan(
   tar_change(
     file_iotable_producer_price_small,
     download_file(
-      url = "https://www.pref.saitama.lg.jp/documents/173111/h27-seisanhyouka-187.xlsx",
+      url = "https://www.pref.chiba.lg.jp/toukei/toukeidata/sangyou/h27/documents/40-h27togo185.xlsx",
       destfile = "_targets/user/iotable/producer_price/small.xlsx"
     ),
     change = "0.1.0",
@@ -19,8 +19,8 @@ target_iotable_producer_price <- tar_plan(
 read_file_iotable_producer_price_small <- function(file) {
   io_table_reader(file) |>
     io_table_read_cells(
-      sheets = "生産者価格評価表(小分類)",
-      rows_exclude = 1:2
+      sheets = "生産者価格評価表",
+      rows_exclude = 1
     ) |>
     io_table_read_headers(
       input_names = c(
@@ -32,12 +32,6 @@ read_file_iotable_producer_price_small <- function(file) {
         "output_sector_name"
       )
     ) |>
-    as_step(mutate)(
-      across(
-        c(input_sector_name, output_sector_name),
-        \(x) str_remove_all(x, "\\s")
-      )
-    ) |>
     io_table_read_sector_names(
       input_sector_name_glue = "{input_sector_code}_{input_sector_name}",
       output_sector_name_glue = "{output_sector_code}_{output_sector_name}"
@@ -47,13 +41,12 @@ read_file_iotable_producer_price_small <- function(file) {
       industry_pattern = "^[0-6]\\d+_",
       value_added_pattern = "家計外消費支出|賃金・俸給|社会保険料|その他の給与及び手当|営業余剰|資本減耗引当|間接税|経常補助金",
       final_demand_pattern = "家計外消費支出|家計消費支出|対家計民間非営利団体消費支出|一般政府消費支出|県内総固定資本形成|在庫純増",
-      export_pattern = "_([移輸]出|輸出（直接購入）)$",
-      import_pattern = "_(（控除）(輸入|輸入（直接購入）|輸入（関税）|輸入（輸入品商品税）)|移入)$",
+      export_pattern = "_([移輸]出)$",
+      import_pattern = "_（控除）[移輸]入$",
       total_pattern = "県内生産額"
     ) |>
     io_table_read_data(
-      value_scale = 1e6,
-      total_tolerance = 1e-3
+      value_scale = 1e6
     ) |>
     end_step()
 }
