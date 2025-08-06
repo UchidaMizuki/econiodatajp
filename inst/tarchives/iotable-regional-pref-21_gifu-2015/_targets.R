7a1962ef657286795cfdc25f24781d2a6aea40c7
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-08-06 23:24:20 +0900

    Add Gifu 2015 regional IO table pipeline
    Introduces a new tarchive for the Gifu 2015 regional input-output table, including a targets pipeline for downloading, reading, and processing producer price data. Adds supporting R scripts, targets configuration, .gitignore, metadata, and R project file for reproducibility and workflow management.

---
+++ inst/tarchives/iotable-regional-pref-21_gifu-2015/_targets.R
@@ -0,0 +1,21 @@
+library(conflicted)
+conflicts_prefer(
+  dplyr::filter(),
+  .quiet = TRUE
+)
+
+library(targets)
+library(tarchetypes)
+library(tarchives)
+
+tar_option_set(
+  packages = c("econioread", "adverbial", "stringr", "tidyr", "dplyr", "purrr"),
+  imports = c("econio", "econioread", "adverbial")
+)
+
+tar_source_archive("econiodatajp")
+tar_source()
+
+tar_plan(
+  target_iotable_producer_price
+)
