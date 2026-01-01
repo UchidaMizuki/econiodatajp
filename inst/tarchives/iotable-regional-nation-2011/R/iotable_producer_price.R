1930215cad36d7bf3cbc14a2db4f836092307444
Uchida Mizuki <uchidamizuki@vivaldi.net>
2026-01-01 15:57:38 +0900

    Add sector and producer price IO table targets for 2011
    Introduces R scripts and target plans for reading, processing, and converting sector and input-output table data for the 2011 regional/national dataset. Adds sector conversion logic, data download steps, and _targets configuration, along with appropriate .gitignore and meta files for the targets pipeline.

---
+++ inst/tarchives/iotable-regional-nation-2011/R/iotable_producer_price.R
@@ -0,0 +1,127 @@
+target_iotable_producer_price <- tar_plan(
+  tar_change(
+    # https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00200603&tstat=000001073129&cycle=0&cycle_facet=cycle&tclass1val=0&metadata=1&data=1
+    file_iotable_producer_price_basic,
+    download_file(
+      url = "https://www.e-stat.go.jp/stat-search/file-download?statInfId=000030137963&fileKind=0",
+      destfile = "_targets/user/iotable/producer_price/basic.xlsx"
+    ),
+    change = "0.1.0",
+    format = "file"
+  ),
+  iotable_producer_price_basic = read_file_iotable_producer_price_basic(
+    file = file_iotable_producer_price_basic
+  ),
+  iotable_producer_price_small = convert_sector_iotable_producer_price_basic(
+    iotable_producer_price_basic = iotable_producer_price_basic,
+    conversion_sector_input = conversion_sector_input,
+    conversion_sector_output = conversion_sector_output,
+    sector_class = "small"
+  ),
+  iotable_producer_price_medium = convert_sector_iotable_producer_price_basic(
+    iotable_producer_price_basic = iotable_producer_price_basic,
+    conversion_sector_input = conversion_sector_input,
+    conversion_sector_output = conversion_sector_output,
+    sector_class = "medium"
+  ),
+  iotable_producer_price_large = convert_sector_iotable_producer_price_basic(
+    iotable_producer_price_basic = iotable_producer_price_basic,
+    conversion_sector_input = conversion_sector_input,
+    conversion_sector_output = conversion_sector_output,
+    sector_class = "large"
+  ),
+  iotable_producer_price_template = convert_sector_iotable_producer_price_basic(
+    iotable_producer_price_basic = iotable_producer_price_basic,
+    conversion_sector_input = conversion_sector_input,
+    conversion_sector_output = conversion_sector_output,
+    sector_class = "template"
+  )
+)
+
+read_file_iotable_producer_price_basic <- function(file) {
+  io_table_reader(file) |>
+    io_table_read_cells(
+      sheets = 1,
+      rows_exclude = 1
+    ) |>
+    io_table_read_headers(
+      input_names = c(
+        "input_sector_code",
+        "input_sector_name"
+      ),
+      output_names = c(
+        "output_sector_code",
+        "output_sector_name"
+      )
+    ) |>
+    io_table_read_sector_names(
+      input_sector_name_glue = "{input_sector_code}_{input_sector_name}",
+      output_sector_name_glue = "{output_sector_code}_{output_sector_name}"
+    ) |>
+    io_table_read_sector_types(
+      competitive_import = TRUE,
+      industry_total_pattern = "内生部門計$",
+      value_added_total_pattern = "粗付加価値部門計$",
+      final_demand_total_pattern = "国内最終需要計$",
+      export_pattern = "輸出",
+      export_total_pattern = "輸出計$",
+      import_pattern = "（控除）(輸入|関税|輸入品商品税)",
+      import_total_pattern = "（控除）輸入計$",
+      total_pattern = "国内生産額$"
+    ) |>
+    io_table_read_data(
+      value_scale = 1e6,
+      check_axes = FALSE
+    ) |>
+    end_step()
+}
+
+convert_sector_iotable_producer_price_basic <- function(
+  iotable_producer_price_basic,
+  conversion_sector_input,
+  conversion_sector_output,
+  sector_class
+) {
+  input_sector_data <- conversion_sector_input |>
+    filter(
+      sector_type %in% c("industry", "value_added"),
+      sector_class_from == "basic",
+      sector_class_to == .env$sector_class
+    ) |>
+    select(sector_name_from, sector_name_to) |>
+    rename(from = sector_name_from, to = sector_name_to) |>
+    add_column(weight = 1)
+
+  output_sector_data <- conversion_sector_output |>
+    filter(
+      sector_type %in% c("industry", "final_demand", "export", "import"),
+      sector_class_from == "basic",
+      sector_class_to == .env$sector_class
+    ) |>
+    select(sector_name_from, sector_name_to) |>
+    rename(from = sector_name_from, to = sector_name_to) |>
+    add_column(weight = 1)
+
+  iotable_producer_price <- iotable_producer_price_basic |>
+    io_reclass(
+      input_sector_data = input_sector_data,
+      output_sector_data = output_sector_data,
+      check_axes = FALSE
+    )
+
+  dim_names <- dimnames(iotable_producer_price)
+  input <- dim_names$input
+  output <- dim_names$output
+  output <- bind_rows(
+    input |>
+      filter(io_sector_type(sector) == "industry"),
+    output |>
+      filter(io_sector_type(sector) != "industry")
+  )
+
+  iotable_producer_price |>
+    dibble::broadcast(dim_names = list(input = input, output = output)) |>
+    dibble::broadcast(dim_names = c("input", "output")) |>
+    replace_na(0) |>
+    io_check_axes()
+}
---
+++ inst/tarchives/iotable-regional-nation-2011/R/sector.R
@@ -0,0 +1,292 @@
+target_sector <- tar_plan(
+  tar_change(
+    # https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00200603&tstat=000001073129&cycle=0&tclass1val=0&metadata=1&data=1
+    file_sector,
+    download_file(
+      url = "https://www.e-stat.go.jp/stat-search/file-download?statInfId=000030137957&fileKind=0",
+      destfile = "_targets/user/sector.xlsx"
+    ),
+    change = "0.1.0",
+    format = "file"
+  ),
+  sector_raw = read_file_sector(
+    file = file_sector
+  ),
+  conversion_sector_input = get_conversion_sector(
+    sector_raw = sector_raw,
+    axis = "input"
+  ),
+  conversion_sector_output = get_conversion_sector(
+    sector_raw = sector_raw,
+    axis = "output"
+  ),
+  sector_input = get_sector(
+    conversion_sector = conversion_sector_input
+  ),
+  sector_output = get_sector(
+    conversion_sector = conversion_sector_output
+  )
+)
+
+read_file_sector <- function(file) {
+  col_names <- c(
+    "output_sector_code_basic_1",
+    "output_sector_code_basic_2",
+    "input_sector_code_basic_1",
+    "input_sector_code_basic_2",
+    "sector_name_basic",
+    "sector_code_small",
+    "sector_name_small",
+    "sector_code_medium",
+    "sector_name_medium",
+    "sector_code_large",
+    "sector_name_large"
+  )
+
+  sector_industry <- readxl::read_excel(
+    file,
+    sheet = "内生部門",
+    col_names = col_names,
+    col_types = "text"
+  ) |>
+    filter(
+      str_detect(output_sector_code_basic_1, "^\\d{4}$") |
+        str_detect(input_sector_code_basic_1, "^\\d{4}$")
+    ) |>
+    mutate(
+      sector_type = case_when(
+        sector_name_basic == "内生部門計" ~ "industry_total",
+        .default = "industry"
+      ),
+      .before = 1
+    )
+
+  sector_final_demand_value_added <- readxl::read_excel(
+    file,
+    sheet = "最終需要部門・粗付加価値部門",
+    col_names = col_names,
+    col_types = "text"
+  ) |>
+    filter(
+      str_detect(output_sector_code_basic_1, "^\\d{4}") |
+        str_detect(input_sector_code_basic_1, "^\\d{4}")
+    ) |>
+    mutate(
+      sector_type = case_when(
+        !is.na(output_sector_code_basic_1) ~ case_when(
+          sector_name_basic == "国内最終需要計" ~ "regional_final_demand_total",
+          sector_name_basic == "国内需要合計" ~ "regional_demand_total",
+          sector_name_basic == "輸出計" ~ "export_total",
+          str_starts(sector_name_basic, "輸出") ~ "export",
+          sector_name_basic == "最終需要計" ~ "final_demand_total",
+          sector_name_basic == "需要合計" ~ "demand_total",
+          sector_name_basic == "（控除）輸入計" ~ "import_total",
+          str_starts(sector_name_basic, "（控除）") ~ "import",
+          sector_name_basic == "最終需要部門計" ~ "final_demand_sector_total",
+          str_starts(sector_name_basic, "商業マージン") ~ "trade_margin",
+          str_starts(sector_name_basic, "貨物運賃") ~ "transport_margin",
+          sector_name_basic == "国内生産額" ~ "total",
+          .default = "final_demand"
+        ),
+        !is.na(input_sector_code_basic_1) ~ case_when(
+          sector_name_basic == "粗付加価値部門計" ~ "value_added_total",
+          sector_name_basic == "国内生産額" ~ "total",
+          .default = "value_added"
+        ),
+      ),
+      .before = 1
+    )
+
+  sector <- bind_rows(
+    sector_industry,
+    sector_final_demand_value_added
+  ) |>
+    mutate(
+      output_sector_code_basic = str_c(
+        output_sector_code_basic_1,
+        output_sector_code_basic_2
+      ),
+      .keep = "unused",
+      .before = output_sector_code_basic_1
+    ) |>
+    mutate(
+      input_sector_code_basic = str_c(
+        input_sector_code_basic_1,
+        input_sector_code_basic_2
+      ),
+      .keep = "unused",
+      .before = input_sector_code_basic_1
+    ) |>
+    mutate(
+      across(
+        c(input_sector_code_basic, output_sector_code_basic),
+        \(x) str_remove(x, "P$")
+      ),
+      across(
+        c(sector_name_small, sector_name_medium, sector_name_large),
+        \(x) str_remove_all(x, "\\s|^（続き）|（\\d／\\d）$")
+      )
+    ) |>
+    fill(
+      sector_code_small,
+      sector_name_small,
+      sector_code_medium,
+      sector_name_medium,
+      sector_code_large,
+      sector_name_large
+    ) |>
+    mutate(
+      output_sector_name_basic = str_c(
+        output_sector_code_basic,
+        sector_name_basic,
+        sep = "_"
+      ),
+      .before = output_sector_code_basic
+    ) |>
+    mutate(
+      input_sector_name_basic = str_c(
+        input_sector_code_basic,
+        sector_name_basic,
+        sep = "_"
+      ),
+      .before = input_sector_code_basic
+    ) |>
+    select(
+      !c(output_sector_code_basic, input_sector_code_basic, sector_name_basic)
+    ) |>
+    unite(
+      "sector_name_small",
+      c(sector_code_small, sector_name_small)
+    ) |>
+    unite(
+      "sector_name_medium",
+      c(sector_code_medium, sector_name_medium)
+    ) |>
+    unite(
+      "sector_name_large",
+      c(sector_code_large, sector_name_large)
+    )
+
+  sector_template <- readxl::read_excel(
+    file,
+    sheet = "13部門分類",
+    col_names = c(
+      "sector_code_large",
+      "sector_name_large",
+      "sector_template_code",
+      "sector_name_template"
+    ),
+    col_types = "text",
+    .name_repair = "minimal"
+  ) |>
+    filter(str_detect(sector_code_large, "^\\d{2}$")) |>
+    fill(
+      sector_template_code,
+      sector_name_template
+    ) |>
+    unite(
+      "sector_name_large",
+      c(sector_code_large, sector_name_large)
+    ) |>
+    unite(
+      "sector_name_template",
+      c(sector_template_code, sector_name_template)
+    ) |>
+    mutate(
+      across(
+        c(sector_name_large, sector_name_template),
+        \(x) str_remove_all(x, "\\s")
+      )
+    )
+
+  sector <- sector |>
+    left_join(
+      sector_template,
+      by = join_by(sector_name_large)
+    ) |>
+    mutate(
+      across(
+        sector_name_template,
+        \(x) {
+          case_match(
+            sector_type,
+            "industry" ~ x,
+            .default = sector_name_large,
+          )
+        }
+      )
+    )
+
+  sector_input <- sector |>
+    drop_na(input_sector_name_basic) |>
+    select(!output_sector_name_basic) |>
+    rename(
+      sector_name_basic = input_sector_name_basic
+    ) |>
+    mutate(
+      across(sector_type, as_factor),
+      across(starts_with("sector_name"), \(x) str_remove_all(x, "\\s"))
+    )
+  sector_output <- sector |>
+    drop_na(output_sector_name_basic) |>
+    select(!input_sector_name_basic) |>
+    rename(
+      sector_name_basic = output_sector_name_basic
+    ) |>
+    mutate(
+      across(sector_type, as_factor),
+      across(starts_with("sector_name"), \(x) str_remove_all(x, "\\s"))
+    )
+
+  list(
+    input = sector_input,
+    output = sector_output
+  )
+}
+
+get_conversion_sector <- function(sector_raw, axis) {
+  sector <- sector_raw[[axis]]
+
+  sector_class <- as_factor(c("basic", "small", "medium", "large", "template"))
+
+  expand_grid(
+    sector_class_from = sector_class,
+    sector_class_to = sector_class
+  ) |>
+    mutate(
+      col_name_sector_class_from = str_c("sector_name_", sector_class_from),
+      col_name_sector_class_to = str_c("sector_name_", sector_class_to)
+    ) |>
+    mutate(
+      data = list(col_name_sector_class_from, col_name_sector_class_to) |>
+        pmap(\(col_name_sector_class_from, col_name_sector_class_to) {
+          tibble(
+            sector_type = sector$sector_type,
+            sector_name_from = sector[[col_name_sector_class_from]],
+            sector_name_to = sector[[col_name_sector_class_to]]
+          )
+        }),
+      .keep = "unused"
+    ) |>
+    unnest(data) |>
+    relocate(sector_type) |>
+    arrange(sector_type) |>
+    distinct()
+}
+
+get_sector <- function(conversion_sector) {
+  conversion_sector |>
+    select(
+      sector_type,
+      sector_class_from,
+      sector_name_from
+    ) |>
+    rename(
+      sector_class = sector_class_from,
+      sector_name = sector_name_from
+    ) |>
+    mutate(
+      across(sector_type, fct_drop)
+    ) |>
+    distinct()
+}
